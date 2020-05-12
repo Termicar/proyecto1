6a3db6c5e547a79512c4308c9f35cde3e5ea8b5f
Termicar <64962383+Termicar@users.noreply.github.com>
2020-05-12 20:57:34 +0200

    tercer commit

--- ejercicio1.java
+++ ejercicio1.java
@@ -7,6 +7,11 @@ public class ejercicio1 {
 		System.out.println("Esto es la primera prueba para Git");
 		
 		System.out.println("Esta es la primera modificacion para el Git");
+		
+		System.out.println("Esta es la segunda modificacion para el Git");
+		
+		/*como el ejercicio dice que haga dos modificaciones mas y actualiece, el anterior Syso es la primera y 
+		este comentario es la segunda modificacion*/
 	}
 
 }
