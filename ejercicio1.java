1737230b29dba18a3750d01ed56677547b87ee57
Termicar <64962383+Termicar@users.noreply.github.com>
2020-05-12 20:49:23 +0200

    segundo commit

--- ejercicio1.java
+++ ejercicio1.java
@@ -5,6 +5,8 @@ public class ejercicio1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Esto es la primera prueba para Git");
+		
+		System.out.println("Esta es la primera modificacion para el Git");
 	}
 
 }
